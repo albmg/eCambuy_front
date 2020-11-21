34645d02ff0471351fdf52ecf3da04b357c591ea
albmg <albmguer@gmail.com>
2020-11-21 15:12:24 +0000

    fix index button

--- pages/index.vue
+++ pages/index.vue
@@ -9,15 +9,12 @@
           <h2 class="second text-center">Locales, Saludables y Sabrosos</h2>
           <div class="text-center mt-12">
             <v-btn color="#ffa83a" dark nuxt to="search">
-              <v-icon left>mdi-eye</v-icon>
-              Busca los productos por localización
+              <v-icon left>mdi-magnify</v-icon>
+              Buscar productos por localización
             </v-btn>
           </div>
         </div>
 
-        <!--<div class="text-center">
-          <h2 class="secon">Últimos Productos</h2>
-        </div>-->
         <div class="mt-2">
           <ProductSlider />
         </div>
@@ -33,12 +30,6 @@
             </div>
           </v-col>
         </div>
-        <!--<div class="text-center mt-12">
-          <v-btn color="#ffa83a" dark nuxt to="productList">
-            <v-icon left>mdi-eye</v-icon>
-            Ver todos los productos
-          </v-btn>
-        </div>-->
       </v-col>
     </v-row>
   </v-container>
@@ -53,6 +44,9 @@ export default {
 </script>
 
 <style scoped>
+* {
+  text-transform: none !important;
+}
 .main-title {
   font-family: 'Roboto Condensed', sans-serif;
   font-size: 40px;
@@ -61,16 +55,9 @@ export default {
 .second {
   font-family: 'Cabin', sans-serif;
   font-size: 28px;
-
-  /* color: #74b49b; */
 }
 .call {
   color: #5c8d89;
   font-family: 'Cabin', sans-serif;
-}
-.secon {
-  font-family: 'Cabin', sans-serif;
-  color: #74b49b;
-  font-size: 32px;
 }
 </style>
